bf4b42a222c05f140712c276258e0896d3fc2896
Adam C Hamlin <achamlin@gmail.com>
2022-08-14 12:28:06 -0400

    Remove nix.conf on uninstall

--- nix/uninstall-nix.sh
+++ nix/uninstall-nix.sh
@@ -2,7 +2,7 @@
 #
 # Uninstall nix. Mostly for dev/test purposes.
 
-rm -rf $HOME/{.nix-channels,.nix-defexpr,.nix-profile,.config/nixpkgs}
+rm -rf $HOME/{.nix-channels,.nix-defexpr,.nix-profile,.config/nixpkgs,.config/nix}
 sudo rm -rf /nix
 
 echo 'Remove the . "$HOME/.nix-profile/etc/profile.d/nix.sh" line in your ~/.profile or ~/.bash_profile, if present'
